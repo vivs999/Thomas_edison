d1f466e545424476a64241df63afc51c9a4d5438
vivs999 <71889870+vivs999@users.noreply.github.com>
2021-05-12 15:06:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function on() {
+    var on_image = "On.png"
+    document.getElementById("bulb").src =on_image; 
+}
+
+function off() {
+    var off_image = "Off.png"
+    document.getElementById("bulb").src =off_image; 
+}
